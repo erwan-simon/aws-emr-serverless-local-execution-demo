085b4af18316cf7970cdca02d3c3057f4ebafe4f
erwan-simon <29917280+erwan-simon@users.noreply.github.com>
2025-01-13 10:15:17 +0100

    removed terraform s3 backend for simplicity

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,13 +2,6 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    key                  = "emr-serverless-local-execution.tfstate"
-    workspace_key_prefix = ""
-    encrypt              = true
-    region               = "eu-west-1"
-    dynamodb_table       = "poc_terraform_backend"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
